a309baa33d5fa4a33d9c8ce3c5e10e18133c048c
Xiao00-arc <servampkuro001@gmail.com>
2025-10-25 13:05:55 +0530

    Update API URL to deployed Render backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://backend-iao4.onrender.com/api',
 });
 
 // This is an interceptor. It runs before every request.
